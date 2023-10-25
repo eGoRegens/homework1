6bb418d46291bb917fcbf76c694efa436bfab3ca
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-25 14:18:50 +0400

    создан метод рассчета треугольника

---
+++ src/main/java/S_triangle.java
@@ -0,0 +1,7 @@
+public class S_triangle {
+    public static void main(String[] args) {
+        int base = 4;
+        int height = 2;
+        System.out.println("Площадь треугольника:"+(base*height)/2);
+    }
+}
