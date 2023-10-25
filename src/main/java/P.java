16b825e3c77d64f17e6f051693416b150def3ace
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-25 14:18:24 +0400

    создан метод рассчета периметра прямоуголника(это для себя)

--- src/main/java/P.java
+++ src/main/java/P.java
@@ -1,9 +1,9 @@
 public class P {
     public static void main(String[] args) {
-        int length1 = 5;
-        int length2 = 4;
+        int length1 = 7;
+        int length2 = 3;
         int width1 = 2;
-        int width2 = 3;
+        int width2 = 5;
         System.out.println("Периметр прямоугольника:"+(length1+length2+width1+width2));
     }
 }
