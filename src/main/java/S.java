65b3c1a74f4a1cc66a6b19c342d8ec78b2203b24
Влад <famasko@ya.ru>
2023-10-25 10:32:48 +0400

    создан метод рассчета площади прямоуголника,с возможностью возможности задать произвольные значения переменных length и width.

--- src/main/java/S.java
+++ src/main/java/S.java
@@ -2,6 +2,6 @@ public class S {
     public static void main(String[] args) {
         int length = 6;
         int width = 3;
-        System.out.println(length*width);
+        System.out.println("Площадь прямоугольника:"+length*width);
     }
 }
