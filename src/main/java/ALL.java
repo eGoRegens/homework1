4ac847a9484ef343f8ff610d7bdb4e8d9233ff1a
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-26 12:51:04 +0400

    добавлен метод расчета площади круга

--- src/main/java/ALL.java
+++ src/main/java/ALL.java
@@ -2,6 +2,7 @@ public class ALL {
     public static void main(String[] args) {
         lengthSquare(4);
         valuesTringle(5, 3);     // 5-основание(a), 3-высота(h)
+        radiusCircle(1.7);
     }
 
     public static void lengthSquare(double length) {
@@ -11,7 +12,13 @@ public class ALL {
 
     public static void valuesTringle(double a, double h) {
         double space = (a*h)/2;
-        System.out.println("Площадь треугольника равняется "+ space);
+        System.out.println("Площадь треугольника равняется - "+ space);
+
+    }
+
+    public static void radiusCircle(double radius) {
+        double space =Math.PI*radius*radius;
+        System.out.println("Площадь круга - "+space);
 
     }
 }
