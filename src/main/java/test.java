565219397fcd1480bfd62fb5dc6c857a75a4ee4c
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-26 16:18:45 +0400

    метод расчета площади квадрата при помощи scanner

---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите значение стороны квадрата:");
+        double x = s.nextDouble();
+        System.out.println("Площадь квадрата равна: "+x*x);
+    }
+}
