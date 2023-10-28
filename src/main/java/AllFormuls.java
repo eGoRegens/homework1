7b60d68abad8ea64393723cc80ebb87aca4750c8
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-28 17:25:40 +0400

    присваивание значения стороне квадрата

--- src/main/java/AllFormuls.java
+++ src/main/java/AllFormuls.java
@@ -1,23 +1,24 @@
 public class AllFormuls {
     public static void main(String[] args) {
-        lengthSquare(4);               //длина стороны квадрата
-        valuesTringle(5, 3);     // 5-основание(a), 3-высота(h)
-        radiusCircle(1.7);
+        double lengthSquare = 10;      //длина стороны квадрата
+        SquareSpace(lengthSquare);
+        TriangleSpace(2.5, 7);     // 2-основание(a), 7-высота(h)
+        CircleSpace(2);         // радиус
     }
 
-    public static void lengthSquare(double length) {
+    public static void SquareSpace(double length) {
         double space = length * length;
         System.out.println("площадь квадрата равняется - " + space);
     }
 
-    public static void valuesTringle(double a, double h) {
+    public static void TriangleSpace(double a, double h) {
         double space = (a * h) / 2;
         System.out.println("Площадь треугольника равняется - " + space);
 
     }
 
-    public static void radiusCircle(double radius) {
-        double space = Math.PI * radius * radius;          //не знаю насколько корректно было бы использовать значение 3.14
+    public static void CircleSpace(double radius) {
+        double space = Math.PI * radius * radius;
         System.out.println("Площадь круга - " + space);
 
     }
