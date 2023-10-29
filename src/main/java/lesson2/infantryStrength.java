85f8940e0510ea30ad10be0a22908ee136d2f292
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-29 21:08:20 +0400

    Численность пехоты. Объявление переменных.

---
+++ src/main/java/lesson2/infantryStrength.java
@@ -0,0 +1,14 @@
+package lesson2;
+
+public class infantryStrength {
+    public static void main(String[] args) {
+        int infantryLannisters = 1500;
+        int orksSaruman = 2000;
+        int totalNumberOfInfantry = infantryLannisters + orksSaruman;
+
+        System.out.println("Численность пехоты у Ланнистеров составляет - " + infantryLannisters + " единиц.");
+        System.out.println("Численность пехоты Сарумана равна " + orksSaruman + " единицам.");
+        System.out.println("Общая численность пехоты Ланнистеров и Сарумана равна " + totalNumberOfInfantry + " единицам.");
+
+    }
+}
