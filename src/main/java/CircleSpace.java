94f8b88d3dfeebd7d141c874bb529cc49041b8e0
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-29 23:10:58 +0400

    Расчет кол-ва воды в стакане. Объявление переменных.

--- src/main/java/CircleSpace.java
+++ src/main/java/CircleSpace.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
+
 public class CircleSpace {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Введите длину радиуса");
         double radius = s.nextDouble();
-        double result = ((radius*radius)*Math.PI);
+        double result = ((radius * radius) * Math.PI);
         System.out.println("Площадь круга - " + result);
     }
 }
---
+++ src/main/java/lesson2/age_difference.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+public class age_difference {
+    public static void main(String[] args) {
+        long ageOfTheUniverse = 13000000000L;
+        long ageOfTheHumans = 100000;
+        long ageDifference = ageOfTheUniverse / ageOfTheHumans;
+
+        System.out.println("Возраст вселенной больше в " + ageDifference + " раз больше,возраста человечества.");
+
+    }
+
+}
